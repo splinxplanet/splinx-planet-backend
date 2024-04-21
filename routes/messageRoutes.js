7a5c567b7adb007832b4c244c3e4c248c7528289
profsain <husseinimudiking@gmail.com>
2024-04-21 15:44:46 +0100

    fix parth error

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -19,13 +19,15 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // endpoint to post new message and store
-router.post('/messages', upload.single("imageFile"), messageController.postMessage);
+router.post('/messages', upload.single("imageFile"), authController, messageController.postMessage);
 
 // endpoint to get userDetails to create chatroom
-router.get('/user/:userId', messageController.getUserDetails);
+router.get('/user/:userId', authController, messageController.getUserDetails);
 
 // endpoint to fetch messages between user
-router.get('/messages/:senderId/:recipientId', messageController.fetchMessages);
+router.get('/messages/:senderId/:recipientId', authController, messageController.fetchMessages);
 
 // endpoint to delete messages
-router.delete('/deleteMessage', messageController.deleteMessage);
+router.delete('/deleteMessage', authController, messageController.deleteMessage);
+
+module.exports = router;
