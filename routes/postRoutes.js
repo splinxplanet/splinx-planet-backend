071dee4d3b4de183e7c6de7d95fda3b97605d919
profsain <husseinimudiking@gmail.com>
2024-05-04 20:04:02 +0100

    fix http method

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,7 +8,7 @@ const postController = require('../controllers/postController');
 // create new post
 router.post('/', postController.createPost);
 // like a post
-router.post('/:postId/like', postController.likePost);
+router.put('/:postId/like', postController.likePost);
 // get all post likes
 router.get('/:postId/likes', postController.getPostLikes);
 // get all post by community id
