ff4c4e9774384feb7d52c3e78a19920823038251
profsain <husseinimudiking@gmail.com>
2024-07-04 17:32:25 +0100

    edit login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 // Get user profile
 exports.getUserProfile = async (req, res) => {
