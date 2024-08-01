1df76d0bff1859c4133cb3094b3cdc6c85716f00
profsain <husseinimudiking@gmail.com>
2024-08-01 04:23:33 +0100

    add request endpoint

--- models/SplinxWallet.js
+++ models/SplinxWallet.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// Transaction Schema
 const transactionSchema = new mongoose.Schema({
   type: {
     type: String,
@@ -16,8 +17,57 @@ const transactionSchema = new mongoose.Schema({
   },
   description: String,
   toWallet: String, // For transfers
+      name: {
+      type: String,
+      required: [true, "name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      trim: true,
+    },
+    currency: {
+      type: String,
+      required: [true, "currency is required"],
+      enum: ["NGN", "USD", "EUR", "GBP"],
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["successful", "pending", "failed"],
+      default: "pending",
+    },
 });
 
+// Request money Schema
+const moneyRequestSchema = new mongoose.Schema({
+  requester: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  requestee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  description: String,
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected'],
+    default: 'pending',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Wallet Schema
 const walletSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
@@ -34,6 +84,7 @@ const walletSchema = new mongoose.Schema({
     default: 0,
   },
   transactions: [transactionSchema],
+  moneyRequests: [moneyRequestSchema],
 }, { timestamps: true });
 
 module.exports = mongoose.model("Wallet", walletSchema);
