bd6300e104226f54b8ab6a77c78becaf11455f7e
profsain <husseinimudiking@gmail.com>
2024-08-24 23:07:57 +0100

    add id to route

--- routes/splinxWalletRoutes.js
+++ routes/splinxWalletRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/create-wallet/:id', walletController.createWallet);
 router.get('/get-wallet/:id', authMiddleware, walletController.getWallet);
-router.post('/fund-wallet', authMiddleware, walletController.fundWallet);
+router.post('/fund-wallet/:id', authMiddleware, walletController.fundWallet);
 router.post('/transfer-funds/:id', authMiddleware, walletController.transferFunds);
 router.post('/make-payment', authMiddleware, walletController.makePayment);
 router.get('/transaction-history/:id', authMiddleware, walletController.getTransactionHistory);
